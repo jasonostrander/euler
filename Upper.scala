32e36634b9607dc2efed71d75c1d022adc3851c6
Jason Ostrander <jason.ostrander@gmail.com>
2011-03-09 09:04:32 -0800

    Rename euler files to follow convention.

--- Upper.scala
+++
@@ -1,9 +0,0 @@
-// code-examples/IntroducingScala/upper3.scala
-
-object Upper {
-  def main(args: Array[String]) = {
-    args.map(_.toUpperCase()).foreach(printf("%s ",_))
-    println("")
-  }
-}
-
